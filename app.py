f13c296cb375491ac25af4ff2b4f49383fbaab5a
christelledongmo <christelledongmo058@gmail.com>
2026-04-18 23:43:36 +0100

    Mise a jour du serveur et html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template,request
-
+import pandas as pd 
+import os 
 app=Flask(__name__)
+DATA_FILE='donnees etudiants.csv'
 
-@pp.route('/', methods=['GET , POST'])
+@app.route('/', methods=['GET' , 'POST'])
 def index():
     if request.method == 'POST':
         nouvelle_donnee={
@@ -12,14 +14,14 @@ def index():
         'stress': request.form.get('stress'),
         'assiduite':request.form.get('assiduite'),
         'moyenne':request.form.get('moyenne')
-     }
-     df = pd.DataFrame([nouvelle_donnee])
-     if not os.path.exists(DATA_FILE):
-        df.to_csv(DATA_FILE, index=False)
-     else:
-        df.to_CSV(DATA_FILE, mode='a', header=False,index=False)
-        
-     return "Merci ! Tes donnees ont ete enregistrees avec succes."
+         }
+        df = pd.DataFrame([nouvelle_donnee])
+        if not os.path.exists(DATA_FILE):
+         df.to_csv(DATA_FILE, index=False)
+        else:
+         df.to_csv(DATA_FILE, mode='a', header=False,index=False)
+
+        return "Merci ! Tes donnees ont ete enregistrees avec succes."
     return render_template("index.html")
 
 if __name__=='__main__':
